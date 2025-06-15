84f84f81bb713480eb6b96d9b3c5616fde077ca0
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2025-06-15 11:50:49 -0400

    Prep work 2

--- index.test.mjs
+++
@@ -1,6 +0,0 @@
-import { myModule } from '@purinton/discord';
-import { jest, test, expect } from '@jest/globals';
-
-test('myModule returns expected string', () => {
-  expect(myModule()).toBe('Hello from template ESM');
-});
